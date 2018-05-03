d1af6afc2de1b90a5d655a7190cfee60b19b1f82
kai930322 <kai930322@outlook.com>
2018-05-04 00:13:27 +0800

    ClasssModel修改

--- application/common/model/Classs.php
+++ application/common/model/Classs.php
@@ -21,12 +21,16 @@ class Classs extends Model
 
     /**
      * 获取表的列表
-     * join(class,mojor,department)
+     * join(class_list,mojor_list,department_list)
+     * 从中间表mid_cmj出发
      */
     public function getClassList()
     {
         $class = Db::name('mid_cmj')->alias('a')
-        ->join('__CLASS_LIST__ c', 'a.c_id = c.class_id')
-        ->select();
+            ->field('c.*', 'd.department_name', 'm.major_name')
+            ->join('__CLASS_LIST__ c', 'a.c_id = c.class_id')
+            ->join('__MAJOR_LIST__ m', 'a.j_id = m.major_id')
+            ->join('__DEPARTMENT_LIST__ d', 'a.m_id = d.department_id')
+            ->select();
     }
 }
